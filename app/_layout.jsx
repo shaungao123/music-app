37629fdd5e3981821800c23a78baf853b7daf88a
Shaun Gao <shaungao80@gmail.com>
2025-08-18 20:58:35 -0700

    fixed minor bug where home could access dashboard

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -17,7 +17,7 @@ const RootLayout = () => {
           <Stack.Screen name='(auth)' options={{headerShown: false}}></Stack.Screen>
           <Stack.Screen name='(dashboard)' options={{headerShown: false}}></Stack.Screen>
 
-          <Stack.Screen name="index" options={{title: 'Home'}}></Stack.Screen>
+          <Stack.Screen name="index" options={{headerShown: false, title: 'Home'}}></Stack.Screen>
         </Stack>
     </>
 

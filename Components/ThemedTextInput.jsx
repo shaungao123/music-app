398604574f68f1eb1c6d09b7e08230b87f242d41
Shaun Gao <shaungao80@gmail.com>
2025-08-24 00:15:41 -0700

    login form ui, no functionality

---
+++ Components/ThemedTextInput.jsx
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, View , TextInput, useColorScheme} from 'react-native'
+import React from 'react'
+import { Colors } from '../constants/Colors'
+
+const ThemedTextInput = ({style, ...props}) => {
+    const colorScheme = useColorScheme()
+    const theme = Colors[colorScheme] ?? Colors.light
+  return (
+    <TextInput
+        style = {[
+            {
+                backgroundColor: theme.uiBackground,
+                color: theme.text,
+                padding: 20,
+                borderRadius: 6
+            },
+            style
+        ]}
+        {...props}
+    />
+  )
+}
+
+export default ThemedTextInput
+
+const styles = StyleSheet.create({})
